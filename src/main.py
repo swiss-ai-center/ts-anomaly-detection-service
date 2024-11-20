c6b650f7aba01c9b1087ed666ceda54350cdd97f
Andrea Petrucci <andrea.petrucci@hefr.ch>
2024-11-20 16:06:46 +0100

    changed docs url

--- src/main.py
+++ src/main.py
@@ -175,7 +175,6 @@ class MyService(Service):
                 ),
             ],
             has_ai=True,
-            # OPTIONAL: CHANGE THE DOCS URL TO YOUR SERVICE'S DOCS
             docs_url="https://docs.swiss-ai-center.ch/reference/services/ts-anomaly-detection/",
         )
         self._logger = get_logger(settings)
